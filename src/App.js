7a84d417b7f327bea0d2d54544d80580f51153c3
John <omnisonic@gmail.com>
2020-05-31 21:23:12 -0700

    added chart 1  css to app.css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
+
 function App() {
+
+  const [data, setApiData] = useState([]);
+
+  console.log('fetch results:', data)
+
+
+  fetch("https://api.exchangeratesapi.io/latest")
+      .then(response => response.json())
+      .then(data => {
+        console.log('data', data);
+        // Check for errors:
+        // if (data.message ===  "Not Found") {
+        //   setResults([]);
+        //   return;
+        // }
+       setApiData(data);
+      });
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div>
+    <div class="flex-container" id="data">
+
     </div>
+   </div>
   );
 }
+
+
 
 export default App;

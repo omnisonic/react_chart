c18067dce5a7ab69255ecd56f3a321e3edf6a12c
John <omnisonic@gmail.com>
2020-05-31 22:11:26 -0700

    converted onclicks alerts to jsx

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 
 
 function App() {
 
-  const [data, setApiData] = useState([]);
+  const [data, setApiData] = useState(0);
 
-  console.log('fetch results:', data)
+  useEffect(() => {
+    console.log('fetch results:', data)
+  });
 
 
   // fetch("https://api.exchangeratesapi.io/latest")
@@ -31,11 +33,11 @@ function App() {
       <div>
           <div className="column-chart">
               <ul className="chart-container">
-                  <li onclick="alert('EUR costs 1.00 EUR')" className="chart-column" style={{ height: '50.0%' }}>EUR</li>
-                  <li onclick="alert('USD costs 0.926 EUR')" className="chart-column" style={{ height: '46.3%' }}>USD</li>
-                  <li onclick="alert('AUD costs 0.594 EUR')" className="chart-column" style={{ height: '29.7%' }}>AUD</li>
-                  <li onclick="alert('GBP costs 1.127 EUR')" className="chart-column" style={{ height: '56.36%' }}>GBP</li>
-                  <li onclick="alert('BRL costs 0.16 EUR')" className="chart-column" style={{ height: '08.0%' }} >BRL</li>
+                  <li onClick={() => alert('EUR costs 1.00 EUR')} className="chart-column" style={{ height: '50.0%' }}>EUR</li>
+                  <li onClick={() => alert('USD costs 0.926 EUR')} className="chart-column" style={{ height: '46.3%' }}>USD</li>
+                  <li onClick={() => alert('AUD costs 0.594 EUR')} className="chart-column" style={{ height: '29.7%' }}>AUD</li>
+                  <li onClick={() => alert('GBP costs 1.127 EUR')} className="chart-column" style={{ height: '56.36%' }}>GBP</li>
+                  <li onClick={() => alert('BRL costs 0.16 EUR')} className="chart-column" style={{ height: '08.0%' }} >BRL</li>
 
               </ul>
           </div>

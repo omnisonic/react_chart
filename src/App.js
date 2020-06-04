0ac701e1ed62dc61a718d4e357b9c30d7232ead4
John <omnisonic@gmail.com>
2020-06-03 17:32:43 -0700

    converted obj to array and filtered apidata to limit number of bars in the graph

--- src/App.js
+++ src/App.js
@@ -1,50 +1,80 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
+const App = () => {
+  const [apiData, setApiData] = useState({});
+  console.log('apidata set', apiData);
+  // const [newData, setnewData] = useState({});
 
 
-function App() {
+  function doFetch() {
+    fetch('https://api.exchangeratesapi.io/latest')
+      .then(response => response.json())
+      .then(data => {
+        console.log('got data back!', data);
+        setApiData(Object.entries(data.rates).filter(innerArray => innerArray[1] <= 5));
+      });
+  }
+ 
+  useEffect(doFetch, []);
 
-  const [data, setApiData] = useState(0);
-
-  useEffect(() => {
-    console.log('fetch results:', data)
-  });
 
 
-  // fetch("https://api.exchangeratesapi.io/latest")
-  //     .then(response => response.json())
-  //     .then(data => {
-  //       console.log('data', data);
-  //       // Check for errors:
-  //       // if (data.message ===  "Not Found") {
-  //       //   setResults([]);
-  //       //   return;
-  //       // }
-  //      setApiData(data);
-  //     });
+  // newData = Object.keys(apiData).filter(function(value) {
+  //   // Countries under 1000000000
+  //   return apiData[value] <= 3;
+  // });
+  
+  // console.log('filtered data:', newData);
 
+  // rates = Object.keys(apiData)
+  //   .filter(rate => rates[rate] <= 3)
+  //   .reduce((a, c) => {a[c] = rates[c]; 
+  //     return a}, {})
+      
+  //     console.log('filtered data:', rates);
+
+
+  // const obj = apiData;
+  // console.log('new obj', obj)
+  // const filteredData = obj.filter(function (elem) {
+  //   return elem.value >= 3
+
+  // });
+  // console.log(filteredData);
+
+  // try this https://stackoverflow.com/questions/2722159/how-to-filter-object-array-based-on-attributes
 
   return (
    <div>
+    {/* <div className="BarChart"> {
+    apiData.map(datum => (
+    <div className="BarChart-bar" style={{height: datum.percentage + "%"}}> {datum.label}
+  </div>
+    ))
+      }
+  </div> */}
+
+
+
+
+
+
+
     <div>
-
       <h2>Exchange Rate Chart</h2>
-      <div>
-          <div className="column-chart">
-              <ul className="chart-container">
-                  <li onClick={() => alert('EUR costs 1.00 EUR')} className="chart-column" style={{ height: '50.0%' }}>EUR</li>
-                  <li onClick={() => alert('USD costs 0.926 EUR')} className="chart-column" style={{ height: '46.3%' }}>USD</li>
-                  <li onClick={() => alert('AUD costs 0.594 EUR')} className="chart-column" style={{ height: '29.7%' }}>AUD</li>
-                  <li onClick={() => alert('GBP costs 1.127 EUR')} className="chart-column" style={{ height: '56.36%' }}>GBP</li>
-                  <li onClick={() => alert('BRL costs 0.16 EUR')} className="chart-column" style={{ height: '08.0%' }} >BRL</li>
-
-              </ul>
-          </div>
+      <div className="column-chart">
+        <ul className="chart-container"> 
+          {
+            Object.entries(apiData).map(([, value]) => (
+              <li onClick={() => alert('1 EUR'  + ' costs ' + value[1] + value[0])} className="chart-column" style={{height: (value[1] * 50) + "%"}}> {value[0]}
+              </li>
+            ))
+          }
+        </ul>
       </div>
-
-</div>
-   </div>
+    </div>
+  </div>
   );
 }
 

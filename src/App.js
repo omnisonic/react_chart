96f48d404b08d687f6558a7a8fb354c930262cfb
John <omnisonic@gmail.com>
2020-06-04 14:28:01 -0700

    added title attribute to bars

--- src/App.js
+++ src/App.js
@@ -12,38 +12,11 @@ const App = () => {
       .then(response => response.json())
       .then(data => {
         console.log('got data back!', data);
-        setApiData(Object.entries(data.rates).filter(innerArray => innerArray[1] <= 4));
+        setApiData(Object.entries(data.rates).filter(innerArray => innerArray[1] <= 5));
       });
   }
  
   useEffect(doFetch, []);
-
-
-
-  // newData = Object.keys(apiData).filter(function(value) {
-  //   // Countries under 1000000000
-  //   return apiData[value] <= 3;
-  // });
-  
-  // console.log('filtered data:', newData);
-
-  // rates = Object.keys(apiData)
-  //   .filter(rate => rates[rate] <= 3)
-  //   .reduce((a, c) => {a[c] = rates[c]; 
-  //     return a}, {})
-      
-  //     console.log('filtered data:', rates);
-
-
-  // const obj = apiData;
-  // console.log('new obj', obj)
-  // const filteredData = obj.filter(function (elem) {
-  //   return elem.value >= 3
-
-  // });
-  // console.log(filteredData);
-
-  // try this https://stackoverflow.com/questions/2722159/how-to-filter-object-array-based-on-attributes
 
   return (
    <div class="container">
@@ -55,7 +28,10 @@ const App = () => {
       <div className="chart-container"> 
         {
           Object.entries(apiData).map(([, value]) => (
-            <div onClick={() => alert('1 EUR'  + ' costs ' + value[1] + value[0])} className="chart-bar" style={{height: (value[1] * 50) + "%"}}> {value[0]}
+            <div title="Click bar for rate info" onClick={() => alert('1 EUR'  + ' costs ' + value[1] + value[0])} 
+            className="chart-bar" 
+            style={{height: (value[1] * 20) + "%"}}> 
+            {value[0]}
             </div>
           ))
         }
@@ -66,7 +42,5 @@ const App = () => {
   </div>
   );
 }
-
-
 
 export default App;
